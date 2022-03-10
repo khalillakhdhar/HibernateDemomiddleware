4a4830da793453b1a30af11d4055e915640263e2
khalil <khalillakhdharatc@gmail.com>
2022-03-10 14:55:51 +0100

    Initial commit

---
+++ src/main/java/com/duranco/HibernateDemomiddlewareApplication.java
@@ -0,0 +1,13 @@
+package com.duranco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateDemomiddlewareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateDemomiddlewareApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/duranco/model/entities/Etudiant.java
@@ -0,0 +1,35 @@
+package com.duranco.model.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+
+@Entity(name = "student")
+public class Etudiant {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	@Column(name = "name", length = 50)
+	@NotBlank
+	private String studentName;
+	@Column(columnDefinition = "Integer default 18", nullable = false)
+	// si default est de type String columnDefinition = "varchar(255) default default 'valeur' "
+	int age;
+	
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	
+	
+	
+}
